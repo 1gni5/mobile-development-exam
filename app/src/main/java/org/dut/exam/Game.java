62dc16335fca05e2aa9a9be7e938d3cd0db88509
1gni5 <1gni5@net-c.com>
2021-03-05 19:37:38 +0100

    Retreive every buttons

--- app/src/main/java/org/dut/exam/Game.java
+++ app/src/main/java/org/dut/exam/Game.java
@@ -2,13 +2,33 @@ package org.dut.exam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game extends AppCompatActivity {
+
+    static final byte NUMBER_OF_BUTTONS = 10;
+
+    GameMode gameMode;
+    ArrayList<Button> allButtons = new ArrayList<Button>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Récupère tout les boutons du layout
+        for(int i = 1; i <= NUMBER_OF_BUTTONS; i++) {
+
+            // Construit l'identifiant et récupère l'id
+            String identifier = "gameButton" + i;
+            int buttonId = getResources().getIdentifier(identifier, "id", getPackageName());
+
+            allButtons.add((Button)findViewById(buttonId));
+        }
     }
 }
