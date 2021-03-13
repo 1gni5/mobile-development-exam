5c0be3d4ba26e4e86cc5910673e3db34b2fc3a03
1gni5 <1gni5@net-c.com>
2021-03-13 16:54:04 +0100

    Code clean-up

--- app/src/main/java/org/dut/exam/Game.java
+++ app/src/main/java/org/dut/exam/Game.java
@@ -2,73 +2,39 @@ package org.dut.exam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Game extends AppCompatActivity {
 
-    static final byte NUMBER_OF_BUTTONS = 10;
-    static final byte MAXIMUM_LEVEL = 7;
-    static final byte DEFAULT_BUTTONS_NUMBER = 3;
+    public static final byte NUMBER_OF_BUTTONS = 10;
+    public static final byte MAXIMUM_LEVEL = 7;
+    public static final byte DEFAULT_BUTTONS_NUMBER = 3;
 
-    GameMode gameMode;
-    byte level, lives;
-    private ArrayList<Button> allButtons = new ArrayList<Button>();
-
-    /**
-     * Créer une sous-liste en tirant aléatoirement dans une liste d'origine.
-     * @param list La liste d'origine
-     * @param numberOfElements Le nombre d'éléments de la sous-liste
-     * @return Sous-liste d'éléments
-     */
-    protected ArrayList<Button> getRandomSubList(ArrayList<Button> list, int numberOfElements) {
-
-        // Prépare la liste de résultat
-        ArrayList<Button> results = new ArrayList<Button>();
-
-        // Tire aléatoirement N boutons
-        Random random = new Random();
-        for(int i = 0; i < numberOfElements; i++) {
-            int index = random.nextInt(list.size());
-            results.add(list.remove(index));
-        }
-
-        return results;
-    }
+    private ArrayList<Button> allGameButtons = new ArrayList<Button>(NUMBER_OF_BUTTONS);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        // Simule le passage d'argument par une activité précèdente
-        level = 7;
-
         // Récupère tout les boutons du layout
         for(int i = 1; i <= NUMBER_OF_BUTTONS; i++) {
-
             // Construit l'identifiant et récupère l'id
             String identifier = "gameButton" + i;
             int buttonId = getResources().getIdentifier(identifier, "id", getPackageName());
 
-            allButtons.add((Button)findViewById(buttonId));
-        }
-
-        // Création d'une sous-liste avec les blocs à allumer
-        ArrayList<Button> activeButtons = getRandomSubList(
-                new ArrayList<Button>(allButtons),
-                level+DEFAULT_BUTTONS_NUMBER
-        );
-
-        for(Button button : activeButtons) {
-            button.setVisibility(View.VISIBLE);
+            allGameButtons.add((Button)findViewById(buttonId));
         }
     }
 }
