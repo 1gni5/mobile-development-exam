02bc58ec1bbb36b43201911d0d9bdb02ac3d3e71
1gni5 <1gni5@net-c.com>
2021-03-15 19:28:17 +0100

    Add heart sprites behaviors

--- app/src/main/java/org/dut/exam/Game.java
+++ app/src/main/java/org/dut/exam/Game.java
@@ -2,31 +2,28 @@ package org.dut.exam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.os.Handler;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.Random;
 
 public class Game extends AppCompatActivity {
 
     public static final byte NUMBER_OF_BUTTONS = 10;
+    public static final byte NUMBER_OF_HEART = 3;
     public static final byte MAXIMUM_LEVEL = 7;
     public static final byte DEFAULT_BUTTONS_NUMBER = 3;
 
     private byte currentLevel = 1;
+    private GameMode gameMode = GameMode.EASY;
 
     private ArrayList<Button> allGameButtons = new ArrayList<>(NUMBER_OF_BUTTONS);
     private ArrayList<Button> activeGameButtons = new ArrayList<>();
@@ -34,7 +31,24 @@ public class Game extends AppCompatActivity {
     private ArrayList<Button> currentSequence = new ArrayList<>();
     private ArrayList<Button> playerSequence = new ArrayList<>();
 
+    private ArrayList<ImageView> heartSprites = new ArrayList<>(NUMBER_OF_HEART);
+
     private FloatingActionButton playButton;
+
+    Hashtable<GameMode, Integer> gameModeToHearts = new Hashtable<>();
+
+    public Game() {
+        /* --- Initialise les HashTable --- */
+
+        // Prépare les 2 listes
+        GameMode[] gameModes = GameMode.values();
+        int[] numberOfHearts = {2, 2, 3, 3};
+
+        // Remplit la hashtable
+        for(int index = 0; index < numberOfHearts.length; index++) {
+            gameModeToHearts.put(gameModes[index], numberOfHearts[index]);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,13 +64,26 @@ public class Game extends AppCompatActivity {
             allGameButtons.add(findViewById(buttonId));
         }
 
+        // Affiche tout les coeurs sur le layout
+        for(int i = 1; i <= gameModeToHearts.get(gameMode); i++) {
+            // Construit l'identifiant et récupère l'id
+            String identifier = "lifeImageView" + i;
+            int heartImageViewId = getResources().getIdentifier(identifier, "id", getPackageName());
+
+            heartSprites.add(findViewById(heartImageViewId));
+        }
+
+        for(ImageView heartSprite : heartSprites) {
+            heartSprite.setVisibility(View.VISIBLE);
+        }
+
         // Active le listener du playButton
         playButton = findViewById(R.id.playButton);
         playButton.setOnClickListener(
                 (view)->{gameStart();}
         );
 
-        // Si la liste des boutons actifs n'est pas remplit
+        // Si la liste des boutons actifs n'est pas remplie
         if (activeGameButtons.size() == 0 ) {
             generatesActiveGameButtons(DEFAULT_BUTTONS_NUMBER + currentLevel);
         }
