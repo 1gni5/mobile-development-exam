1ea07c65b2583db7dc54c785981ef693d7ac5425
1gni5 <1gni5@net-c.com>
2021-03-22 11:57:48 +0100

    Add DatePickerDialogue
    Create a DatePickerDialogue when BirthDay editText is clicked. Format selected date and update birthday text.

--- app/src/main/java/org/dut/exam/Register.java
+++ app/src/main/java/org/dut/exam/Register.java
@@ -2,7 +2,15 @@ package org.dut.exam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Récupère les champs
+        EditText birthDate = (EditText)findViewById(R.id.birthDateEditText);
+
+        // Ajoute les listeners
+        birthDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Register.this);
+                datePickerDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(year, month, day);
+
+                        birthDate.setText(DateFormat.getDateInstance().format(calendar.getTime()));
+                    }
+                });
+                datePickerDialog.show();
+            }
+        });
     }
 }
